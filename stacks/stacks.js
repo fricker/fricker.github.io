a1f6df2ccd54d4c5132dfd52191d7d138638c8e1
Scott Anderson <sarcastic.guy@gmail.com>
2014-03-22 16:00:12 -0700

    initial stacks module

---
+++ stacks/stacks.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var MBD.Stacks = angular.module('mbd.stacks', []);
+    
+MBD.Stacks.controller('stackController', [function() {
+}]);
+
+MBD.Stacks.controller('cardController', [function() {
+}]);
