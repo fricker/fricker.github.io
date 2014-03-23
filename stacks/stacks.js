ed2dba656a5a7b9e8aa57df04514263dc13ffcba
Scott Anderson <sarcastic.guy@gmail.com>
2014-03-22 18:11:16 -0700

    info logging

--- stacks/stacks.js
+++ stacks/stacks.js
@@ -1,9 +1,21 @@
 'use strict';
 
+console.log("initializing stacks lib ...");
+
 var MBD.Stacks = angular.module('mbd.stacks', []);
     
 MBD.Stacks.controller('stackController', [function() {
+	console.log('-> stackController');
 }]);
 
 MBD.Stacks.controller('cardController', [function() {
+	console.log('-> cardController');
+}]);
+
+MBD.Stacks.controller('stackListController', [function() {
+	console.log('-> stackListController');
+}]);
+
+MBD.Stacks.controller('cardListController', [function() {
+	console.log('-> cardListController');
 }]);
